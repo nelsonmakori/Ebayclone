4a47b9a811150bf4c90f09044e4e7a86c4d237f9
nelsonmakori <nelsonmakori6@gmail.com>
2023-11-06 14:51:59 -0700

    trying to add js for first time with cart quantity

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+// document.body.style.backgroundImage = "url('purchasejordan.images/airjordan2.jpg')";
+
+
+function goToNextImage(){
+    const slide = document.querySelectorAll(".slide-images");
+slide[0].style.backgroundImage = "url('purchasejordan.images/airjordan2.jpg')";
+}
+
+// alert("allegra");
+
+function goToPrevImage(){
+    const jordan=document.querySelectorAll(".slide-images");
+    jordan[0].style.backgroundImage= "url('purchasejordan.images/airjordan4.jpg')"
+}
+
